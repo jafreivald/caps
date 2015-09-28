e923e1fec3df4a17d71c5b79ee002296f16bce5e
Joseph Freivald <joseph@freivald.com>
2015-09-28 19:34:46 -0400

    Initial create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CAPS::Application.config.secret_token = 'e613138ffce78943bf52e5301a6e340736e27afa2707cc1043ec91f6358f6bc78cc22d6920dffaff63b71ad5253b0c2aa30277df1ec7153a6fe72956d1eb2b04'
